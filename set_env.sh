f707d9516487fe869079d07bb14f87587678e086
QuocHuyDo <quochuydo.cn@gmail.com>
2024-11-13 00:19:11 +0700

    update cdocker file

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,21 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgres
-export POSTGRES_HOST=cdr.cuvcdz1gzpvb.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.cebvyau4ttec.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=test-685038841062-dev
+export AWS_BUCKET=myproject3-s3-bucket
 export AWS_REGION=us-east-1
-export AWS_PROFILE=udacity
+export AWS_PROFILE=project3
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+
+
+setx POSTGRES_USERNAME "postgres"
+setx POSTGRES_PASSWORD "postgres"
+setx POSTGRES_HOST "postgres.cebvyau4ttec.us-east-1.rds.amazonaws.com"
+setx POSTGRES_DB "postgres"
+setx AWS_BUCKET "myproject3-s3-bucket"
+setx AWS_REGION "us-east-1"
+setx AWS_PROFILE "project3"
+setx JWT_SECRET "testing"
+setx URL "http://localhost:8100"
